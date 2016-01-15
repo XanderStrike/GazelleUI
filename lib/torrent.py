bb6470ab4c19c1ef853d14d7d514eeb3f31c2fa7
Alexander Standke <xanderstrike@gmail.com>
2016-01-15 11:01:54 -0800

    use urllib2 to download torrents instead of wget

--- lib/torrent.py
+++ lib/torrent.py
@@ -2,7 +2,7 @@ import database as database
 import wat as wat
 import settings as settings
 
-import os
+import urllib2
 
 def queue(id):
   database.update('insert into torrents(id, added, downloaded) values ("' + id + '", datetime("now"), 0)')
@@ -10,8 +10,20 @@ def queue(id):
 def download_all():
   torrents = database.row_fetch('select * from torrents where downloaded = 0')
   for t in torrents:
-    download_link = wat.download_link(t[0])
-    download_path = settings.get('torrent')[1]
-    os.system("wget -bq \"" + download_link + "\" -O " + download_path + t[0] + ".torrent")
-    print "Downloaded " + t[0] + ".torrent"
-    database.update('update torrents set downloaded = 1 where id = "' + t[0] + '"')
+    download_torrent(t[0])
+
+def download_torrent(torrent_id):
+  download_link = wat.download_link(torrent_id)
+  download_path = settings.get('torrent')[1]
+  save_to = download_path + torrent_id + ".torrent"
+
+  opener = urllib2.build_opener()
+  opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+  torrent = opener.open(download_link).read()
+
+  output = open(save_to,'wb')
+  output.write(torrent)
+  output.close()
+
+  print "Downloaded " + torrent_id + ".torrent"
+  database.update('update torrents set downloaded = 1 where id = "' + torrent_id + '"')
