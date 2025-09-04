67f7e297248f44897e0971687aab233a9a5bfb01
Alex Standke <xanderstrike@gmail.com>
2025-09-03 19:49:41 -0700

    Update manifest, add service worker and update layout with new assets paths

---
+++ assets/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'gazelleui-v1';
+const urlsToCache = [
+  '/',
+  '/assets/style.css',
+  '/assets/normalize.css',
+  '/assets/skeleton.css',
+  '/assets/script.js',
+  '/assets/logo.png',
+  '/android-icon-192x192.png',
+  '/favicon-32x32.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
