872899bf8655df228ea16c0a5e162d7c1a3bbcee
Alex Standke <xanderstrike@gmail.com>
2025-09-03 22:34:52 -0700

    replace basicauth with session based authentication

--- GazelleUI.py
+++ GazelleUI.py
@@ -4,10 +4,10 @@
 import os
 import sys
 
-from flask import Flask, request, jsonify, render_template, redirect, send_from_directory, Response
+from flask import Flask, request, jsonify, render_template, redirect, send_from_directory, Response, session
 from flask_apscheduler import APScheduler
 
-from lib.auth import requires_auth
+from lib.auth import login_required, check_auth, needs_auth
 import lib.database as database
 import lib.settings as settings
 import lib.wat as wat
@@ -29,6 +29,12 @@ class Config(object):
 
 app = Flask(__name__)
 app.config.from_object(Config())
+app.secret_key = os.urandom(12)
+
+# Make session available in all templates
+@app.context_processor
+def inject_session():
+    return dict(session=session)
 
 # if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
 scheduler = APScheduler()
@@ -41,8 +47,29 @@ database.init()
 
 
 # Routes
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if not needs_auth():
+        return redirect('/')
+    
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if check_auth(username, password):
+            session['logged_in'] = True
+            return redirect('/')
+        else:
+            return render_template('login.html', error='Invalid credentials')
+    
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    session.pop('logged_in', None)
+    return redirect('/login')
+
 @app.route("/")
-@requires_auth
+@login_required
 def index():
   setting = settings.get('what_credentials')
   if setting[1] == None or setting[1] == '':
@@ -51,41 +78,41 @@ def index():
   return render_template('index.html', torrents=torrents, userinfo=database.userinfo())
 
 @app.route("/artist")
-@requires_auth
+@login_required
 def artist():
   query = request.args['q']
   results = wat.get_artist(query)
   return render_template('artist.html', results=results, userinfo=database.userinfo())
 
 @app.route("/browse")
-@requires_auth
+@login_required
 def browse():
   query = request.args['q']
   results = wat.browse(query)
   return render_template('browse.html', results=results, query=query, userinfo=database.userinfo())
 
 @app.route("/label")
-@requires_auth
+@login_required
 def label():
   query = request.args['q']
   results = wat.label(query)
   return render_template('browse.html', results=results, query=query, userinfo=database.userinfo())
 
 @app.route("/want", methods=['POST'])
-@requires_auth
+@login_required
 def want():
   torrent.queue(json.loads(request.form['data']))
   return "<button class='button' disabled>Snatched!</button>"
 
 @app.route("/group_info")
-@requires_auth
+@login_required
 def group_info():
   group_id = request.args['id']
   results = wat.get_group(group_id)
   return render_template('group_info.html', group_info=results)
 
 @app.route("/settings", methods=['GET', 'POST'])
-@requires_auth
+@login_required
 def settings_path():
   output = {'message':None,'class':None}
   if request.method == 'POST':
@@ -94,25 +121,25 @@ def settings_path():
   return render_template('settings.html', settings=settings.get_all(), message=output['message'], message_class=output['class'], userinfo=database.userinfo())
 
 @app.route("/snatches")
-@requires_auth
+@login_required
 def snatches():
   torrents = torrent.get_all()
   return render_template('snatches.html', torrents=torrents, userinfo=database.userinfo())
 
 @app.route('/delete_sub/<int:sub_id>')
-@requires_auth
+@login_required
 def delete_sub(sub_id):
   database.delete_sub(sub_id)
   return redirect('/subscriptions')
 
 @app.route('/create_sub', methods=['POST'])
-@requires_auth
+@login_required
 def create_sub():
   autofetch.create_subscription(request.form)
   return redirect('/subscriptions')
 
 @app.route("/subscriptions")
-@requires_auth
+@login_required
 def subscriptions():
   subs = database.subscriptions()
   return render_template('subscriptions.html', subs=subs, userinfo=database.userinfo())
--- lib/auth.py
+++ lib/auth.py
@@ -1,6 +1,4 @@
-# Original: http://flask.pocoo.org/snippets/8/
-
-from flask import request, Response
+from flask import request, Response, session, redirect, url_for, render_template
 from functools import wraps
 import lib.settings as settings
 
@@ -8,21 +6,16 @@ def check_auth(username, password):
     creds = settings.get('webui_credentials')
     return username == creds[1] and password == creds[2]
 
-def authenticate():
-    return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
+def login_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not needs_auth() and (not auth or not check_auth(auth.username, auth.password)):
-            return authenticate()
+        if not needs_auth():
+            return f(*args, **kwargs)
+        if not session.get('logged_in'):
+            return redirect('/login')
         return f(*args, **kwargs)
     return decorated
 
 def needs_auth():
     creds = settings.get('webui_credentials')
-    return creds[1] == None or creds[1] == ''
+    return creds[1] != None and creds[1] != ''
